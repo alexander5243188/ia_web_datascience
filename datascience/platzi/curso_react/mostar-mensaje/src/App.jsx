4e585ad0335e6493edad394c0cad3815a231b759
Alexander <alexander5243188@yahoo.es>
2025-11-26 21:17:24 -0400

    Creamos: props basico mostrando un mensaje

---
+++ datascience/platzi/curso_react/mostar-mensaje/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Mensaje from './components/Mensaje'
+
+function App() {
+
+
+  return (
+    <>      
+      <div className="card">        
+        <Mensaje saludo={"Calmate todo cambiara"} />
+      </div>      
+    </>
+  )
+}
+
+export default App
---
+++ datascience/platzi/curso_react/mostar-mensaje/src/components/Mensaje.jsx
@@ -0,0 +1,9 @@
+const Mensaje = ({saludo}) =>{
+    return(
+        <section>
+            <h2>{saludo}</h2>
+        </section>
+    )
+}
+
+export default Mensaje;
